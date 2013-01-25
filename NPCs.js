cc656c2c8f7e29fad40a01997a4545e47c568d35
fae2day <aqiu@andrew.cmu.edu>
2013-01-25 14:17:04 -0500

    Update NPCs.js
    added more dialogue, made the item hide somewhere else.

--- NPCs.js
+++ NPCs.js
@@ -1,21 +1,33 @@
 /* NPCs.js
  * 		Contains NPC-related functions.
 */
-
 function npcAction(npc,item){
-  if(item==undefined)
-		talk(npc);
+
+	if(npc.done===true){
+		if(item==undefined)
+			talk(npc);
+		else{
+			hasItem(npc,item)
+		};
+		numTurns--;
+	}
 	else{
-		hasItem(npc,item)
-	};
+		printDialogue(npc.name+": "+"Thank you for your help! You're awesome.");
+	}
 }
 function talk(npc){
+	
 	printDialogue(npc.name+": "+npc.hintDialogue[0]);
 }
 
 function hasItem(npc,item){
-	if (item.name===npc.itemID)
+	
+	if (item.name===npc.itemID){
 		printDialogue(npc.name+": "+npc.acceptDialogue [0]);
+		item.x=600;
+		item.y=600; // hides the item from the page
+		npc.done=true;
+	}
 	else
 		printDialogue(npc.name+": "+npc.rejectDialogue [0]);
 }
