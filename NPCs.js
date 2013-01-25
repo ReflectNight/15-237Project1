b7211da227f30c9ccab348d97a51eb8a20f3f671
fae2day <aqiu@andrew.cmu.edu>
2013-01-25 13:50:00 -0500

    Update NPCs.js
    adding basic functions to the NPC

--- NPCs.js
+++ NPCs.js
@@ -2,4 +2,20 @@
  * 		Contains NPC-related functions.
 */
 
-// NOT YET IMPLEMENTED
+function npcAction(npc,item){
+  if(item==undefined)
+		talk(npc);
+	else{
+		hasItem(npc,item)
+	};
+}
+function talk(npc){
+	printDialogue(npc.name+": "+npc.hintDialogue[0]);
+}
+
+function hasItem(npc,item){
+	if (item.name===npc.itemID)
+		printDialogue(npc.name+": "+npc.acceptDialogue [0]);
+	else
+		printDialogue(npc.name+": "+npc.rejectDialogue [0]);
+}
