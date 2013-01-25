a1b6fc6f323582c025403c772acdb1bd7a576678
fae2day <aqiu@andrew.cmu.edu>
2013-01-25 13:58:20 -0500

    Update items.js
    changed the dialogue for item

--- items.js
+++ items.js
@@ -3,8 +3,8 @@
 */
 
 function itemAction(){
-  
-  if(arguments[0]==undefined)
+	
+	if(arguments[0]===undefined)
 		pickup(arguments[0]);
 	else{
 		switchitem(arguments[0],arguments[1]);
@@ -15,19 +15,14 @@ function pickUp(item){
   
 	item.x=inventory.x;
 	item.y=inventory.y;
-	itemDialogue=("you have picked up "+item.name+"!");//this could be randomized
-  // dialogue should be made into a new js file. some other time.
+	printDialogue("you have picked up "+item.name+"!");//this could be randomized
 }
 
 function switchitem(onHand,newItem){
+	
 	onHand.x=newItem.x;
 	onHand.y=newItem.y;
 	newItem.x=inventory.x;
 	newItem.y=inventory.y;
-  
-	itemDialogue=("You dropped "+onHand.name+" and picked up "+newItem.name+"!");
-}
-
-function itemDialogue(dialogue){
-
+	printDialogue("You dropped "+onHand.name+" and picked up "+newItem.name+"!");
 }
