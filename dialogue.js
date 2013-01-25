ba04894c6ace4b98d64c1ac9afbf0d7b85beb73e
fae2day <aqiu@andrew.cmu.edu>
2013-01-25 10:13:13 -0800

    Create dialogue.js
    added basic functionality 

---
+++ dialogue.js
@@ -0,0 +1,18 @@
+/*
+This is to be the dialogue files
+
+*/
+
+function drawDialogue(){
+
+  ctx.fillRect(600,150,0,450);
+  //ctx.save();
+}
+
+function printDialogue(txt){
+  
+  ctx.font="20pt Monospace";
+  ctx.fillStyle = "white";
+  ctx.fillText(txt,10,500);
+  //press space to continue
+}
